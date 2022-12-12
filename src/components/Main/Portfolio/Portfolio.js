a61111461640669c23e2c757385d59ec2b16e738
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-12-12 21:17:24 +0500

    Portfolio add link

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -7,18 +7,18 @@ export default function Portfolio({ email }) {
       <h4 className="portfolio__name">Портфолио</h4>
       <nav className="portfolio__list">
         <div className="portfolio__block">
-          <a href="https://github.com/gatalsky-dv/movies-explorer-frontend/tree/level-2" className="portfolio__link">Статичный сайт</a>
-          <a href="https://github.com/gatalsky-dv/movies-explorer-frontend/tree/level-2" className="portfolio__img" />
+          <a href="https://github.com/gatalsky-dv/how-to-learn" className="portfolio__link">Статичный сайт</a>
+          <a href="https://github.com/gatalsky-dv/how-to-learn" className="portfolio__img" />
         </div>
         <div className="line portfolio__line"></div>
         <div className="portfolio__block">
-          <a href="https://github.com/gatalsky-dv/movies-explorer-frontend/tree/level-2" className="portfolio__link">Адаптивный сайт</a>
-          <a href="https://github.com/gatalsky-dv/movies-explorer-frontend/tree/level-2" className="portfolio__img" />
+          <a href="https://github.com/gatalsky-dv/russian-travel" className="portfolio__link">Адаптивный сайт</a>
+          <a href="https://github.com/gatalsky-dv/russian-travel" className="portfolio__img" />
         </div>
         <div className="line portfolio__line"></div>
         <div className="portfolio__block">
-          <a href="https://github.com/gatalsky-dv/movies-explorer-frontend/tree/level-2" className="portfolio__link">Одностраничное приложение</a>
-          <a href="https://github.com/gatalsky-dv/movies-explorer-frontend/tree/level-2" className="portfolio__img" />
+          <a href="https://github.com/gatalsky-dv/react-mesto-api-full" className="portfolio__link">Одностраничное приложение</a>
+          <a href="https://github.com/gatalsky-dv/react-mesto-api-full" className="portfolio__img" />
         </div>
       </nav>
 
